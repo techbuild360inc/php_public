5741c91e0c786e3d5a1b0877cf8e36ea0404373d
Build360 Inc <104673940+techbuild360inc@users.noreply.github.com>
2023-02-10 14:51:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
 echo "tokenWA GetEnv -> ".getEnv('tokenWA');
-echo " tokenWA Var -> ".$tokenWA;
+echo " tokenWA Local -> ".getEnv('tokenWALocal');
 
+foreach (getenv() as $settingKey => $settingValue) {
+   echo ' settingKey-> '.$settingKey.' -> '.$settingValue."<hr/>";
+}
 ?>
